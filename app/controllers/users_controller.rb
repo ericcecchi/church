f230c41588523a675e1900cc6e8d66630921bdcf
Eric Cecchi <eric.cecchi@gmail.com>
2012-03-01 04:09:52 -0600

    Fixed vanity URLs bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.first(conditions: {username: params[:username].downcase } )
     if not @user
       flash[:alert] = "The page you are looking for does not exist."
       redirect_to :root
     end
-    @user = User.first(conditions: {username: params[:username].downcase } )
   end
 end
